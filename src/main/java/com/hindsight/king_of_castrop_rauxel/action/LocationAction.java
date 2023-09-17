004690e6200455cd46500d34b326ba7af346b8b8
Kim Goetzke <kbgoetzke@gmail.com>
2023-09-17 12:06:49 +0100

    WIP - Handle threading errors more gracefully

--- src/main/java/com/hindsight/king_of_castrop_rauxel/action/LocationAction.java
+++ src/main/java/com/hindsight/king_of_castrop_rauxel/action/LocationAction.java
@@ -8,8 +8,10 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import static com.hindsight.king_of_castrop_rauxel.characters.Player.*;
 
@@ -24,16 +26,25 @@ public class LocationAction implements Action {
 
   @Override
   public void execute(Player player, List<Action> actions) {
-    generateSettlementOnSeparateThread();
+    var poiLeft = player.getCurrentPoi();
+    var settlementFuture = generateSettlementOnSeparateThread();
     ProgressBar.displayProgress(player.getCurrentLocation(), location);
-    setPlayerState(player);
-    player.setCurrentPoi(location.getDefaultPoi());
+    try {
+      settlementFuture.get();
+      setPlayerState(player);
+      player.setCurrentPoi(location.getDefaultPoi());
+    } catch (ExecutionException | InterruptedException e) {
+      Thread.currentThread().interrupt();
+      System.out.printf(
+          "%nSeems like you didn't know the way because you ended up where you started.%n");
+      setPlayerState(player);
+      player.setCurrentPoi(poiLeft);
+    }
   }
 
-  private void generateSettlementOnSeparateThread() {
+  private Future<?> generateSettlementOnSeparateThread() {
     try (ExecutorService executor = Executors.newSingleThreadExecutor()) {
-      executor.submit(() -> location.generate());
-      executor.shutdown();
+      return executor.submit(() -> location.generate());
     }
   }
 
